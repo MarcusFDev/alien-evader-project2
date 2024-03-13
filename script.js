856f186ad3fadae9b9d1645761bafb47af6e3b2d
Marcus F <marcusf.dev@gmail.com>
2024-03-13 22:12:23 +0000

    Changed: Phaser to find game window div when DOM is loaded for site design.

--- script.js
+++ script.js
@@ -6,21 +6,26 @@ import Game from '/scenes/Game.js'
 
 import GameOver from '/scenes/GameOver.js'
 
-export default new Phaser.Game ({
-    type: Phaser.CANVAS,
-    width: 1920,
-    height: 1080,
-    scene: [ GameStart, Game, GameOver],
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: {
-                x: -10
-            },
-            debug: false
-        }
-    },
+function initializeGame() {
+    return new Phaser.Game({
+        type: Phaser.CANVAS,
+        width: 1920,
+        height: 1080,
+        scene: [GameStart, Game, GameOver],
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: {
+                    x: -10
+                },
+                debug: false
+            }
+        },
+        parent: 'gameContainer' // Specify the parent container for the Phaser canvas
+    });
+}
+
+// Call the initializeGame function when the DOM content is loaded
+document.addEventListener('DOMContentLoaded', () => {
+    initializeGame();
 });
-
-
-
