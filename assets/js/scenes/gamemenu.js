4a2a5b8a5dd5bcc8c5b6b13c0f0be148fe85f43e
Marcus F <marcusf.dev@gmail.com>
2024-03-25 15:29:28 +0000

    Added: gamemenu scene for 425px code.

--- assets/js/scenes/gamemenu.js
+++ assets/js/scenes/gamemenu.js
@@ -14,58 +14,81 @@ export default class gamemenu extends Phaser.Scene {
         // Loads the game start background
         this.load.image('gamemenuscr', 'assets/images/game-assets/gamemenu-screen.webp');
 
+        this.load.image('gamemenuscrtwo', 'assets/images/game-assets/gamemenu-smallsize.webp');
+
         this.load.audio('backgroundMusic', 'assets/game-audio/game-background-sound.mp3');
 
     }
 
     create() {
-        // The Game Menu image
-        this.add.image(0, 0, 'gamemenuscr').setOrigin(0, 0);
 
-        // Start button becomes visible & Menu button is hidden
+        const screenWidth = window.innerWidth;
+        const screenHeight = window.innerHeight;
+    
+        if (window.innerWidth <= 425) {
+    
+            const background = this.add.image(screenWidth / 2, screenHeight / 2, 'gamemenuscrtwo').setOrigin(0.5);
+    
+            const scaleFactor = 0.1;
+            background.setScale(scaleFactor);
+    
+            const startButton = document.querySelector('[data-type="start-btn"]');
+            startButton.classList.remove('hidden');
+    
+            const gameHtpButton = document.querySelector('[data-type="gameHtp-btn"]');
+            gameHtpButton.classList.remove('hidden');
+    
+            const gameMenuButton = document.querySelector('[data-type="gameMenu-btn"]');
+            gameMenuButton.classList.add('hidden');
+    
+            const gameRestartButton = document.querySelector('[data-type="gameRestart-btn"]');
+            gameRestartButton.classList.add('hidden');
+    
+            const gameGoBackButton = document.querySelector('[data-type="gameGoBack-btn"]');
+            gameGoBackButton.classList.add('hidden');
+    
+            const howToPlayDiv = document.querySelector('[data-type="howtoplay-list"]');
+            howToPlayDiv.classList.add('hidden');
+    
+            const gameScoring = document.querySelector('[data-type="gameScore"]');
+            gameScoring.classList.add('hidden');
+        } else {
+            // The Game Menu image
+            this.add.image(0, 0, 'gamemenuscr').setOrigin(0, 0);
+    
+            const startButton = document.querySelector('[data-type="start-btn"]');
+            startButton.classList.remove('hidden');
+    
+            const gameHtpButton = document.querySelector('[data-type="gameHtp-btn"]');
+            gameHtpButton.classList.remove('hidden');
+    
+            const gameMenuButton = document.querySelector('[data-type="gameMenu-btn"]');
+            gameMenuButton.classList.add('hidden');
+    
+            const gameRestartButton = document.querySelector('[data-type="gameRestart-btn"]');
+            gameRestartButton.classList.add('hidden');
+    
+            const gameGoBackButton = document.querySelector('[data-type="gameGoBack-btn"]');
+            gameGoBackButton.classList.add('hidden');
+    
+            const howToPlayDiv = document.querySelector('[data-type="howtoplay-list"]');
+            howToPlayDiv.classList.add('hidden');
+    
+            const gameScoring = document.querySelector('[data-type="gameScore"]');
+            gameScoring.classList.add('hidden');
+        }
+        
+    
+        // Button Event listeners for scene changes
         const startButton = document.querySelector('[data-type="start-btn"]');
-        startButton.classList.remove('hidden');
-
+        this.addButtonEventListeners(startButton, 'game');
+    
         const gameHtpButton = document.querySelector('[data-type="gameHtp-btn"]');
-        gameHtpButton.classList.remove('hidden');
-
-        const gameMenuButton = document.querySelector('[data-type="gameMenu-btn"]');
-        gameMenuButton.classList.add('hidden');
-
-        const gameRestartButton = document.querySelector('[data-type="gameRestart-btn"]');
-        gameRestartButton.classList.add('hidden');
-
-        const gameGoBackButton = document.querySelector('[data-type="gameGoBack-btn"]');
-        gameGoBackButton.classList.add('hidden');
-
-        const howToPlayDiv = document.querySelector('[data-type="howtoplay-list"]');
-        howToPlayDiv.classList.add('hidden');
-
-        // Score Div hidden on this scene
-
-        const gameScoring = document.querySelector('[data-type="gameScore"]');
-        gameScoring.classList.add('hidden')
-
-        this.addButtonClickListener(startButton, 'game');
-        this.addButtonClickListener(gameHtpButton, 'game-htp');
-
+        this.addButtonEventListeners(gameHtpButton, 'game-htp');
+        
         // User Toggle Audio functionality
         const muteButton = document.querySelector('[data-type="audioToggle"]');
-        muteButton.addEventListener('click', () => {
-            this.isMuted = !this.isMuted;
-            this.backgroundMusic.setMute(this.isMuted);
-
-            // Toggle visibility of mute buttons
-            const audioOnBtn = document.getElementById("audioOnBtn");
-            const audioOffBtn = document.getElementById("audioOffBtn");
-            if (this.isMuted) {
-                audioOnBtn.classList.add("hidden");
-                audioOffBtn.classList.remove("hidden");
-            } else {
-                audioOffBtn.classList.add("hidden");
-                audioOnBtn.classList.remove("hidden");
-            }
-        });
+        this.addAudioToggleEventListeners(muteButton);
 
         // Begins to play background music
         if (!this.backgroundMusic || !this.backgroundMusic.isPlaying) {
@@ -77,10 +100,47 @@ export default class gamemenu extends Phaser.Scene {
             this.backgroundMusic.play();
         }
     }
-
-    addButtonClickListener(button, sceneKey) {
+    
+    addButtonEventListeners(button, sceneKey) {
+        // Click event listener for mouse devices
         button.addEventListener('click', () => {
             this.scene.start(sceneKey);
         });
+    
+        // Touch event listener for touchscreens devices
+        button.addEventListener('touchstart', (event) => {
+            event.preventDefault();
+            
+            this.scene.start(sceneKey);
+        });
+    }
+
+    addAudioToggleEventListeners(button) {
+        // Click event listener for mouse devices
+        button.addEventListener('click', () => {
+            this.toggleAudio();
+        });
+
+        // Touch event listener for touchscreens devices
+        button.addEventListener('touchstart', (event) => {
+            event.preventDefault();
+            this.toggleAudio();
+        });
+    }
+
+    toggleAudio() {
+        this.isMuted = !this.isMuted;
+        this.backgroundMusic.setMute(this.isMuted);
+
+        // Toggle visibility of mute buttons
+        const audioOnBtn = document.getElementById("audioOnBtn");
+        const audioOffBtn = document.getElementById("audioOffBtn");
+        if (this.isMuted) {
+            audioOnBtn.classList.add("hidden");
+            audioOffBtn.classList.remove("hidden");
+        } else {
+            audioOffBtn.classList.add("hidden");
+            audioOnBtn.classList.remove("hidden");
+        }
     }
 }
